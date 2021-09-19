0fff5f7b2c3e71cff72fcb670c434eb8d6c83e1d
jiminy <jiminy0410@gmail.com>
2021-09-19 20:31:45 +0200

    er kan op de aarde worden geklikt

--- Clusius College Game/Assets/Scripts/Aarde.cs
+++ Clusius College Game/Assets/Scripts/Aarde.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Aarde : MonoBehaviour
 {
+    Collider col;
+    public Camera mainCamera;
     // Start is called before the first frame update
     void Start()
     {
-        
+        col = GetComponent<BoxCollider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.touches[0];
+            Vector3 clickpunt = touch.position;
+            RaycastHit hit;
+            Ray ray = mainCamera.ScreenPointToRay(clickpunt);
+
+            if (Physics.Raycast(ray, out hit) && hit.collider.tag == "Player")
+            {
+                if (touch.phase == TouchPhase.Began)
+                {
+                    Debug.Log("aarde is geklikt");
+                }
+            }
+        }
     }
 }
