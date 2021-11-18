8149cca3a912603cf8406d02bf8bca477436d4aa
Jiminy <jiminy0410@gmail.com>
2021-11-18 15:01:45 +0100

    player profile ui af

--- Clusius College Game/Assets/playerProfile.cs
+++ Clusius College Game/Assets/playerProfile.cs
@@ -6,20 +6,33 @@ using UnityEngine.UI;
 public class playerProfile : MonoBehaviour
 {
     [SerializeField] float totalExp;
+    [SerializeField] float totalPlant;
     [SerializeField] float nextLevelExp;
     [SerializeField] float PreviusLevelExp;
     [SerializeField] float CurrentLevelExp;
     [SerializeField] float PlayerLevel;
     [SerializeField] string name;
-    string[] firstName = { "cute","long","big","tiny","red","blue"};
-    string[] secondName = { "sad","strong","lazy","toasted", "disrupted", "super"};
-    string[] thirdName = { "dragon","box","farmer","scarecrow","panda","pumkin"};
+    string[] firstName = { "cute","long","big","tiny","red","blue","fast","rocky","mad","happy"};
+    string[] secondName = { "sad","strong","lazy","toasted", "disrupted", "super","sandy","round","yellow","cool","normal"};
+    string[] thirdName = { "dragon","box","farmer","scarecrow","panda","pumkin","egg","chicken","dude","girl","horse","dog"};
     [SerializeField] TMPro.TextMeshProUGUI levelText;
     [SerializeField] TMPro.TextMeshProUGUI ExpText;
+    [SerializeField] TMPro.TextMeshProUGUI PlantText;
     [SerializeField] TMPro.TextMeshProUGUI nameText;
     [SerializeField] Slider sliderExp;
 
-    void earnExp(float amount)
+    private void Start()
+    {
+        changeName();
+        earnExp(0);
+    }
+
+    public void earnPlant(float amount)
+    {
+        totalPlant += amount;
+        ExpText.text = totalPlant.ToString();
+    }
+    public void earnExp(float amount)
     {
         totalExp += amount;
         nextLevelExp = 100 * Mathf.Sqrt(PlayerLevel);
